0005d134632f5e303e378225d09e31fb2d7b6db9
DeriyamsiRitonga <deri21076ti@student.nurulfikri.ac.id>
2022-12-10 20:11:31 +0700

    Pertemuan 10

---
+++ synchronous.js
@@ -0,0 +1,7 @@
+console.log("Deri Membuka Bowser Google Chrome.");
+
+setTimeout(() => {
+    console.log("Downloading 1 Hours....");
+    console.log("Download complete");
+}, 5000);
+console.log("Deri membuka Youtube.");
